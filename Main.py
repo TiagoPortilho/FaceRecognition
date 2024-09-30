63bb589a985c910a37f35bf8a0c3aa723e245d20
Tiago <tiagoportilho52@gmail.com>
2024-09-29 22:34:49 -0300

    Camera working with OpenCV

---
+++ Main.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+#import face_recognition
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video_capture.read()
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
